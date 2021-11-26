d12d22f7d0c660a6fea62a3d84d7a210afa9c99c
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2021-11-26 13:43:50 +0300

    Added src/

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        int currentSum = 100;
+        int refillSum = 220;
+        int bonus = 1;
+        int amount = 100;
+        int totalSum = currentSum + refillSum;
+        int countBonus = refillSum*bonus/amount;
+        int result = totalSum + countBonus;
+        if (refillSum>1000) {
+            System.out.println("Сумма на счете:" + result + "рублей");
+            System.out.println("Количество начисленных бонусов:" + countBonus + "рублей");
+        } else {
+            System.out.println("Сумма на счете:" + totalSum + "рублей");
+            System.out.println("Количество начисленных бонусов: 0 рублей");
+        }
+    }
+}

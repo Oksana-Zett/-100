5db9683b87d161e8c98707b26e6caa1d63bb58f7
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2021-12-23 14:32:27 +0300

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int currentSum = 100;
         int refillSum = 220;
         int bonus = 1;
         int amount = 100;
         int totalSum = currentSum + refillSum;
-        int countBonus = refillSum*bonus/amount;
+        int countBonus = refillSum * bonus / amount;
         int result = totalSum + countBonus;
-        if (refillSum>1000) {
+        if (refillSum > 1000) {
             System.out.println("Сумма на счете:" + result + "рублей");
             System.out.println("Количество начисленных бонусов:" + countBonus + "рублей");
         } else {
